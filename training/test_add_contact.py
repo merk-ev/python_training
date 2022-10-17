22a3985f650bb08b919a54552c091cd947ce9439
Aleksandr <xmerkushevx@gmail.com>
2022-10-17 17:11:11 +0300

    Добавлены комментарии в код

--- training/test_add_contact.py
+++ training/test_add_contact.py
@@ -9,57 +9,52 @@ class TestAddContat(unittest.TestCase):
 
     def test_add_contat(self):
         driver = self.driver
+        # open home page
         driver.get("http://localhost/addressbook/")
+        # login
         driver.find_element_by_name("user").clear()
         driver.find_element_by_name("user").send_keys("admin")
         driver.find_element_by_name("pass").clear()
         driver.find_element_by_name("pass").send_keys("secret")
         driver.find_element_by_xpath("//input[@value='Login']").click()
+        # add new contact
         driver.find_element_by_link_text("add new").click()
-        driver.find_element_by_name("firstname").clear()
+        # fill name
         driver.find_element_by_name("firstname").send_keys("Test")
-        driver.find_element_by_name("middlename").clear()
         driver.find_element_by_name("middlename").send_keys("Testovich")
-        driver.find_element_by_name("lastname").clear()
         driver.find_element_by_name("lastname").send_keys("Testoviy")
-        driver.find_element_by_name("nickname").clear()
         driver.find_element_by_name("nickname").send_keys("Tester78")
-        driver.find_element_by_name("title").clear()
+        # fill about
         driver.find_element_by_name("title").send_keys("Title")
-        driver.find_element_by_name("company").clear()
         driver.find_element_by_name("company").send_keys("Company")
-        driver.find_element_by_name("address").clear()
+        # fill contacts
         driver.find_element_by_name("address").send_keys("Address")
-        driver.find_element_by_name("address").clear()
         driver.find_element_by_name("address").send_keys("Address")
-        driver.find_element_by_name("home").clear()
         driver.find_element_by_name("home").send_keys("+7123456789")
-        driver.find_element_by_name("mobile").clear()
         driver.find_element_by_name("mobile").send_keys("+7777777777")
-        driver.find_element_by_name("work").clear()
         driver.find_element_by_name("work").send_keys("+6666666666")
-        driver.find_element_by_name("fax").clear()
         driver.find_element_by_name("fax").send_keys("+6666666665")
-        driver.find_element_by_name("email").clear()
         driver.find_element_by_name("email").send_keys("e-mail@e.mail")
-        driver.find_element_by_name("email2").clear()
         driver.find_element_by_name("email2").send_keys("-")
-        driver.find_element_by_name("email3").clear()
         driver.find_element_by_name("email3").send_keys("--")
-        driver.find_element_by_name("homepage").clear()
         driver.find_element_by_name("homepage").send_keys("home.apge")
+        # fill Bday
         driver.find_element_by_name("bday").send_keys("30")
         driver.find_element_by_name("bmonth").send_keys("February")
         driver.find_element_by_name("byear").send_keys("1990")
         driver.find_element_by_name("aday").send_keys("30")
         driver.find_element_by_name("amonth").send_keys("February")
         driver.find_element_by_name("ayear").send_keys("2020")
+        # choose group
         driver.find_element_by_name("new_group").send_keys("Test 1")
+        # fill additional
         driver.find_element_by_name("address2").send_keys("non")
         driver.find_element_by_name("phone2").send_keys("empty")
         driver.find_element_by_name("notes").send_keys("designer from SF")
         driver.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+        # go to home
         driver.find_element_by_link_text("home").click()
+        # logout
         driver.find_element_by_link_text("Logout").click()
     
     def is_element_present(self, how, what):
