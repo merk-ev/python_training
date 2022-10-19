e4d670e91a8990df6049e32b5fb8cc477df7c9d6
Aleksandr <xmerkushevx@gmail.com>
2022-10-19 17:56:27 +0300

    Создана общая фикстура для тестов в корневом каталоге - conftest.py Добавлены тесты для удаления контактов и групп

--- training/test/test_add_contact.py
+++ training/test/test_add_contact.py
@@ -4,7 +4,7 @@ from training.model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login()
+    app.session.login(username="admin", password="secret")
     app.contact.add_new(Contact(firstname="TEST", middlename="Test middle", lastname="Test Last",
                                 nickname="TTTT", title="TITlE", company="Company Name", address="RF", home="44-55",
                                 mobile="+7-888", work="41-87", fax="47-88", email="e@m.ail", email2="--", email3="---",
@@ -15,7 +15,7 @@ def test_add_contact(app):
 
 
 def test_add_empty_contact(app):
-    app.session.login()
+    app.session.login(username="admin", password="secret")
     app.contact.add_new(Contact(firstname="", middlename="", lastname="", nickname="", title="", company="",
                                 address="", home="", mobile="", work="", fax="", email="", email2="", email3="",
                                 homepage="", bday="", bmonth="", byear="", aday="", amonth="", ayear="",
--- training/test/test_add_group.py
+++ training/test/test_add_group.py
@@ -4,12 +4,12 @@ from training.model.group import Group
 
 
 def test_add_group(app):
-    app.session.login()
+    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="Test 2", header="header 2", footer="footer 2"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
-    app.session.login()
+    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
