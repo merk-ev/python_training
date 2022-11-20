afb48ea05a1ee5c3879e4dc161b0588eafa0e0d5
Aleksandr <xmerkushevx@gmail.com>
2022-11-20 15:03:12 +0300

    Добавлено сравнение списка групп для: - добавления; - удаления; - редактирования

--- training/test/test_add_group.py
+++ training/test/test_add_group.py
@@ -4,7 +4,13 @@ from training.model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="Test 2", header="header 2", footer="footer 2"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
--- training/test/test_edit_group.py
+++ training/test/test_edit_group.py
@@ -5,9 +5,15 @@ from training.model.group import Group
 def test_edit_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test edit name 1", header="test edit name 2", footer="test edit name 3"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first(Group(name="edit name"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 def test_edit_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test edit header 1", header="test edit header 2", footer="test edit header 3"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first(Group(header="edit header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
