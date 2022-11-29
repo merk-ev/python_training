953080003d4c3b2329a92c12b65de86c64f84daa
Aleksandr <xmerkushevx@gmail.com>
2022-11-29 14:30:48 +0300

    Добавлена проверка телефонов. работает

--- training/fixture/contact.py
+++ training/fixture/contact.py
@@ -1,6 +1,7 @@
 # Helper for contacts
 
 from training.model.contact import Contact
+import re
 
 
 class ContactHelper:
@@ -123,12 +124,60 @@ class ContactHelper:
             wd = self.app.wd
             self.go_home()
             self.contact_cache = []
-            for element in wd.find_elements_by_name('entry'):
-                id = element.find_element_by_name('selected[]').get_attribute('value')
-                firstname = element.find_element_by_xpath('td[3]').text
-                lastname = element.find_element_by_xpath('td[2]').text
-                self. contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id))
+            for row in wd.find_elements_by_name('entry'):
+                cells = row.find_elements_by_tag_name('td')
+                firstname = cells[2].text
+                lastname = cells[1].text
+                id = cells[0].find_element_by_tag_name('input').get_attribute('value')
+                all_phones = cells[5].text.splitlines()
+                self.contact_cache.append(Contact(id=id, firstname=firstname, lastname=lastname, home=all_phones[0],
+                                                  mobile=all_phones[1], work=all_phones[2], phone2=all_phones[3]))
         return list(self.contact_cache)
 
     contact_cache = None
 
+    def open_contact_to_edit_by_index(self, index):
+        wd = self.app.wd
+        self.go_home()
+        row = wd.find_elements_by_name('entry')[index]
+        cell = row.find_elements_by_tag_name('td')[7]
+        cell.find_element_by_tag_name('a').click()
+
+    def open_contact_view_by_index(self, index):
+        wd = self.app.wd
+        self.go_home()
+        row = wd.find_elements_by_name('entry')[index]
+        cell = row.find_elements_by_tag_name('td')[6]
+        cell.find_element_by_tag_name('a').click()
+
+    def get_contact_info_from_edit_page(self, index):
+        wd = self.app.wd
+        self.open_contact_to_edit_by_index(index)
+        firstname = wd.find_element_by_name('firstname').get_attribute('value')
+        lastname = wd.find_element_by_name('lastname').get_attribute('value')
+        id = wd.find_element_by_name('id').get_attribute('value')
+        homephone = wd.find_element_by_name('home').get_attribute('value')
+        workphone = wd.find_element_by_name('work').get_attribute('value')
+        mobilephone = wd.find_element_by_name('mobile').get_attribute('value')
+        secondaryphone = wd.find_element_by_name('phone2').get_attribute('value')
+        return Contact(firstname=firstname, lastname=lastname, id=id, home=homephone, work=workphone,
+                       mobile=mobilephone, phone2=secondaryphone)
+
+    def get_contact_from_view_page(self, index):
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        text = wd.find_element_by_id("content").text
+        homephone = re.search('H: (.*)', text).group(1)
+        workphone = re.search('W: (.*)', text).group(1)
+        mobilephone = re.search('M: (.*)', text).group(1)
+        secondaryphone = re.search('P: (.*)', text).group(1)
+        return Contact(home=homephone, mobile=mobilephone, work=workphone, phone2=secondaryphone)
+
+    def clear(self, string):
+        return re.sub('[() +/*-]', '', string)
+
+    def merge_phones_like_on_home_page(self, contact):
+        return '\n'.join(filter(lambda x: x != '',
+                                map(lambda x: self.clear(x),
+                                    filter(lambda x: x is not None,
+                                           [contact.home, contact.work, contact.mobile, contact.phone2]))))
--- training/model/contact.py
+++ training/model/contact.py
@@ -7,7 +7,7 @@ class Contact:
     def __init__(self, firstname=None, middlename=None, lastname=None, nickname=None, title=None, company=None,
                  address=None,  home=None, mobile=None, work=None, fax=None, email=None, email2=None, email3=None,
                  homepage=None, bday=None, bmonth=None, byear=None, aday=None, amonth=None, ayear=None,
-                 new_group=None,  address2=None, phone2=None, notes=None, id=None):
+                 new_group=None,  address2=None, phone2=None, notes=None, id=None, all_phones=None):
         self.firstname = firstname
         self.middlename = middlename
         self.lastname = lastname
@@ -34,6 +34,7 @@ class Contact:
         self.phone2 = phone2
         self.notes = notes
         self.id = id
+        self.all_phones = all_phones
 
     def __repr__(self):
         return '%s:%s:%s' % (self.id, self.firstname, self.lastname)
