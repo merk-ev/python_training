ba8564a4744a843da82b505c8554b058fc9f375a
Aleksandr <xmerkushevx@gmail.com>
2022-12-03 02:12:12 +0300

    Добавлены параметры для запуска через командую строку с разных бразуеров

--- training/fixture/application.py
+++ training/fixture/application.py
@@ -7,12 +7,20 @@ from training.fixture.group import GroupHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox(firefox_binary='C:\\Program Files\\Mozilla Firefox\\firefox.exe')
-        self.wd.implicitly_wait(2)
+    def __init__(self, browser, base_url):
+        if browser == 'firefox':
+            self.wd = webdriver.Firefox(firefox_binary='C:\\Program Files\\Mozilla Firefox\\firefox.exe')
+        elif browser == 'chrome':
+            self.wd = webdriver.Chrome()
+        elif browser == 'ie':
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+        self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.contact = ContactHelper(self)
         self.group = GroupHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -23,7 +31,7 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get('http://localhost/addressbook/')
+        wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
