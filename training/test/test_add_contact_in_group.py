a8368bfac946d5943694310eb0ad5f6005677e20
Aleksandr <xmerkushevx@gmail.com>
2023-02-09 00:40:52 +0300

    task 22 fixed again

--- training/test/test_add_contact_in_group.py
+++ training/test/test_add_contact_in_group.py
@@ -5,16 +5,24 @@ import random
 
 def test_add_contact_in_group(app, orm, db):
     if len(db.get_contact_list()) == 0:
-        app.contact.create(Contact(firstname="TEST-NAME", middlename="TEST-MIDDLE", lastname="TEST-LAST", bday="01",
-                                   bmonth="JANUARY", byear="2000"))
+        app.contact.add_new(Contact(firstname='TEST', middlename='Test middle', lastname='Test Last',
+                                nickname='TTTT', title='TITlE', company='Company Name', address='RF', home='44-55',
+                                mobile='+7-888', work='41-87', fax='47-88', email='e@m.ail', email2='--', email3='---',
+                                homepage='home.page', bday='22', bmonth='May', byear='1984', aday='15',
+                                amonth='March', ayear='2020', new_group='Test2', address2='street', phone2='non',
+                                notes='designer from streets'))
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="test", header="group test", footer="test group"))
     all_groups = db.get_group_list()
     group = random.choice(all_groups)
     contacts_for_add_in_group = orm.get_contacts_not_in_group(group)
     if contacts_for_add_in_group == []:
-        app.contact.add(Contact(firstname="TEST-NAME", middlename="TEST-MIDDLE", lastname="TEST-LAST", bday="01",
-                                   bmonth="JANUARY", byear="2000"))
+        app.contact.add_new(Contact(firstname='TEST', middlename='Test middle', lastname='Test Last',
+                                nickname='TTTT', title='TITlE', company='Company Name', address='RF', home='44-55',
+                                mobile='+7-888', work='41-87', fax='47-88', email='e@m.ail', email2='--', email3='---',
+                                homepage='home.page', bday='22', bmonth='May', byear='1984', aday='15',
+                                amonth='March', ayear='2020', new_group='Test2', address2='street', phone2='non',
+                                notes='designer from streets'))
         contact_for_add_in_group = orm.get_contacts_not_in_group(group)[0]
     else:
         contact_for_add_in_group = random.choice(contacts_for_add_in_group)
--- training/test/test_del_contact_from_group.py
+++ training/test/test_del_contact_from_group.py
@@ -8,8 +8,12 @@ def test_delete_contact_from_group(app, db, orm):
     groups = db.get_group_list()
     group = random.choice(groups)
     if len(db.get_contact_list()) == 0:
-        app.contact.create(Contact(firstname="TEST-NAME", middlename="TEST-MIDDLE", lastname="TEST-LAST", bday="01",
-                                   bmonth="JANUARY", byear="2000"))
+        app.contact.add_new(Contact(firstname='TEST', middlename='Test middle', lastname='Test Last',
+                                nickname='TTTT', title='TITlE', company='Company Name', address='RF', home='44-55',
+                                mobile='+7-888', work='41-87', fax='47-88', email='e@m.ail', email2='--', email3='---',
+                                homepage='home.page', bday='22', bmonth='May', byear='1984', aday='15',
+                                amonth='March', ayear='2020', new_group='Test2', address2='street', phone2='non',
+                                notes='designer from streets'))
     contacts = db.get_contact_list()
     contact = random.choice(contacts)
     if len(orm.get_contacts_in_group(group)) == 0:
